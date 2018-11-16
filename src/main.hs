8672d917c4f39f7bec531c4964980a33cbbb3dc0
lopezfjose <jflopezfernandez@gmail.com>
2018-11-16 15:56:03 -0500

    Tokenizer is working

--- src/main.hs
+++ src/main.hs
@@ -2,17 +2,65 @@
 module Main where
 
 import System.IO
-import Control.Monad
-import Text.Parsec
-import Text.Parsec.Expr
-import Text.Parsec.Language
-import qualified Text.Parsec.Token as Token
+import System.Environment
+import Data.Char
+-- import Control.Monad
+-- import Text.Parsec
+-- import Text.Parsec.Expr
+-- import Text.Parsec.Language
+-- import qualified Text.Parsec.Token as Token
 
-data Token
+data Operator = Plus
+              | Minus
+              | Times
+              | Div
+              | Modulo
+              | Negation
+              | Exponentiation
+              | Assignment
+        deriving (Show, Eq)
+
+operator :: Char -> Operator
+operator c | c == '+' = Plus
+           | c == '-' = Minus
+           | c == '*' = Times
+           | c == '/' = Div
+           | c == '%' = Modulo
+           | c == '!' = Negation
+           | c == '^' = Exponentiation
+           | c == '=' = Assignment
+           | otherwise = error $ "Unknown operator input: " ++ [c]
+
+operatorToString :: Operator -> String
+operatorToString Plus           = "+"
+operatorToString Minus          = "-"
+operatorToString Times          = "*"
+operatorToString Div            = "/"
+operatorToString Modulo         = "%"
+operatorToString Negation       = "!"
+operatorToString Exponentiation = "^"
+operatorToString Assignment     = "="
+
+data Token = TokenOperator Operator
+           | TokenIdentifier String
+           | TokenNumber Int
+        deriving (Show, Eq)
+
+showOperatorContent :: Token -> String
+showOperatorContent (TokenOperator op) = operatorToString op
+showOperatorContent (TokenIdentifier str) = str
+showOperatorContent (TokenNumber n) = show n
+
 data Expression
 
 tokenize :: String -> [Token]
-tokenize = undefined
+tokenize [] = []
+tokenize (c:cs)
+    | elem c "+/*/%!^=" = TokenOperator (operator c) : tokenize cs
+    | isDigit c = TokenNumber (digitToInt c) : tokenize cs
+    | isAlpha c = TokenIdentifier [c] : tokenize cs
+    | isSpace c = tokenize cs
+    | otherwise = error $ "Cannot tokenize " ++ [c]
 
 parse :: [Token] -> Expression
 parse = undefined
@@ -20,9 +68,12 @@ parse = undefined
 evaluate :: Expression -> Double
 evaluate = undefined
 
+--
 
 main :: IO ()
 main = do
-    line <- getLine
-    putStrLn line
-    main
+    print $ tokenize " 1 + 4 / x"
+    print $ tokenize "!x"
+    print $ tokenize "y=8^2"
+    print $ tokenize "y = 3x/2 + 7"
+    print $ tokenize "y = ln x"
